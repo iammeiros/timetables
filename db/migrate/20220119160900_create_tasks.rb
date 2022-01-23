cea07050de6291de414c41c1ef5ded0805671cf7
meiros <devmeiros@gmail.com>
2022-01-23 11:03:40 +0100

    initialize

---
+++ db/migrate/20220119160900_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.date :start_at
+      t.date :dateline
+
+      t.timestamps
+    end
+  end
+end
